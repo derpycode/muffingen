b312401718c7cc26f39e35f2cc1ae935aca8bb13
Andy Ridgwell <derp@derpy.co.uk>
2019-10-11 08:14:47 -0700

    -

--- muffingen_settings_BLANK.m
+++ muffingen_settings_BLANK.m
@@ -14,14 +14,20 @@
 %
 % PARAM NAME & DEFAULT VALUE   % [FORMAT] BRIEF DESCRIPTION
 %
-% *** EXPERIMENT INPUT AND OUTPUT *************************************** %
+% *** CONFIG NAME AND MAIN INPUT SETTINGS ******************************* %
 %
 par_wor_name='worblank';       % ['worblank'] 8-char (output) config name
-par_gcm='';                    % [''] input format/GCM name
+par_gcm='';                    % [''] input format
 par_expid='';                  % [''] input experiment/data name
+%
+% *** FILE PATHS ******************************************************** %
+%
 par_pathin='INPUT.EXAMPLES';   % ['EXAMPLES.INPUT'] path to input dir
 par_pathout='OUTPUT.EXAMPLES'; % ['EXAMPLES.OUTPUT'] path to output dir
 opt_outputdir=false;           % [false/true] ask for output directory?
+%
+% *** GCM netCDF FILENAMES ********************************************** %
+%
 par_nc_topo_name  = '';        % [''] optional .nc name
 par_nc_mask_name  = '';        % [''] optional .nc name
 par_nc_axes_name  = '';        % [''] optional .nc name
@@ -29,24 +35,30 @@ par_nc_atmos_name = '';        % [''] optional .nc name
 par_nc_ocean_name = '';        % [''] optional .nc name
 par_nc_coupl_name = '';        % [''] optional .nc name
 %
-% *** GRID RESOLUTION & REGRIDDING CONTROLS ***************************** %
+% *** GRID RESOLUTION *************************************************** %
 %
 par_max_i=36;                  % [36] # grid cells in longitude dir (i)
 par_max_j=36;                  % [36] # grid cells in latitude  dir (j)
-par_max_k=16;                  % [36] # depth leves in ocean
+par_max_k=16;                  % [16] # depth levels in ocean
+opt_equalarea=true;            % [false/true] equal area grid?
+%
+% *** REGRIDDING SETTINGS *********************************************** %
+%
 par_max_D=5000.0;              % [5000.0] max grid depth (m)
-par_lon_off=-180.0;            % [-180.0] longitude offset of grid start
+par_sur_D=0.0;                 % [0.0] reference surface depth
 par_min_Dk=2;                  % [2] minimum ocean depth (as # levels)
 par_min_k=3;                   % [1] maximum ocean depth (k value)
-par_sur_D=0.0;                 % [0.0] reference surface depth
+par_lon_off=-180.0;            % [-180.0] longitude offset of grid start
 par_A_frac_threshold=0.50;     % [0.5] fractional area threshold for 'land'
-opt_equalarea=true;            % [false/true] equal area grid?
-opt_highresseds=false;         % [false/true] create 2x res sediment grid
-par_runoffopt=0;               % [0/1] run-off generation option
+par_mask_mask_name = '';       % [''] mask to change land/ocean features
 par_sedsopt=0;                 % [0/1/2] sediment re-gridding option
+opt_highresseds=false;         % [false/true] create 2x res sediment grid
+%
+% *** BOUNDARY CONDITION SETTINGS *************************************** %
+%
+par_runoffopt=0;               % [0/1] run-off generation option
 par_tauopt=1;                  % [0/1/2] zonal windstress generation option
 par_age=0.0;                   % [0.0] optional age of paleo configuration
-par_mask_mask_name = '';       % [''] mask to change land/ocean features
 %
 % *** OPTIONS -- MAIN *************************************************** %
 %
